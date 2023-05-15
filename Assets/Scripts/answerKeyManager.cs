ba972243b5f3eebd151c140b316904fbd95f3c4d
skyla! <skylatran03@gmail.com>
2023-05-14 17:11:29 -0700

    cleaning file

--- Assets/Scripts/answerKeyManager.cs
+++ Assets/Scripts/answerKeyManager.cs
@@ -5,38 +5,17 @@ using UnityEngine;
 public class answerKeyManager : MonoBehaviour
 {
     // variables
-    private tiles[] levelTiles;
     tiles currSelect;
 
     void Start() {
-        // assuming levels are always 22 x 22 = 484 tiles in total
-        levelTiles = new tiles[484];
+
     }
 
     // called once per frame
     void Update() {
         // check if the user clicked a tile
         userClick();
-
-        // check if the puzzle is completed
-        /* if (complete()) {
-            Debug.Log("puzzled solved!!");
-        } */
     }
-
-    // check if the puzzle is completed
-    /* bool complete() {
-        // for every tile in the puzzle check the answer is correct
-        for (int i = 0; i < levelTiles.Length; i++) {
-            // if one tile doesn't match, not complete return false
-            if (levelTiles[i].correctAnswer != levelTiles[i].selected) {
-                return false;
-            }
-        }
-
-        // all tiles match their answer key, return true
-        return true;
-    } */
 
     // when a user clicks something
     void userClick() {
@@ -56,6 +35,7 @@ public class answerKeyManager : MonoBehaviour
 
     }
 
+    // get the tile the user clicked
     void getObject() {
         RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
         if (hit.collider.gameObject.tag == "tiles") {
