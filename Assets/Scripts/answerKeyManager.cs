61687674eab6ee314e9a8d1087c001f79379bac5
skyla! <skylatran03@gmail.com>
2023-05-16 20:45:34 -0700

    so many things

--- Assets/Scripts/answerKeyManager.cs
+++ Assets/Scripts/answerKeyManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class answerKeyManager : MonoBehaviour
 {
@@ -18,6 +19,7 @@ public class answerKeyManager : MonoBehaviour
         userClick();
         if (complete()) {
             Debug.Log("puzzle solved!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 
---
+++ Assets/Scripts/mainMenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    public void playGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
